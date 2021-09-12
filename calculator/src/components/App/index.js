06d7b6a12521272dbaf38416a12267a1433415e0
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-12 13:10:24 -0400

    Added width,height,background, and color. Imported styling css file to app.
    Included width, height, background, color, and imported these elements to our app using the import method. These styling elements are to imitate the actual iPhone's calculator styling.

--- calculator/src/components/App/index.js
+++ calculator/src/components/App/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import "./App.css";
+
 const App = () => {
-  return <div className="App">Hello from App</div>;
+  return (
+    <div className="App">
+      <div className="top">7:77</div>
+      <div className="display">0</div>
+      <div className="buttons">AC</div>
+      <div className="bottom">-</div>
+    </div>
+  );
 };
 
 export default App;
