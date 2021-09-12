cf33590d08761bc285d252fbfe32f05cfd61fff0
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-12 16:25:42 -0400

    Styled Interface & Add 1st button
    Styled the structure of elements for this application in a similar style that would match the iPhone's calculator. The first button was created to set a foundation of multiple buttons.

--- calculator/src/components/App/index.js
+++ calculator/src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Button from "../Button";
 import "./App.css";
 
 const App = () => {
@@ -7,7 +8,9 @@ const App = () => {
     <div className="App">
       <div className="top">7:77</div>
       <div className="display">0</div>
-      <div className="buttons">AC</div>
+      <div className="buttons">
+        <Button content="AC" />
+      </div>
       <div className="bottom">-</div>
     </div>
   );
---
+++ calculator/src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Button.css";
+
+const Button = ({ content }) => {
+  return <div className="Button">{content}</div>;
+};
+
+export default Button;
