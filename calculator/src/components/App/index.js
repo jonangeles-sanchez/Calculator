01cc5f275f0df68a575e89c5a1c33d5c6be13b1f
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-14 16:51:09 -0400

    Implemented basic math arithmetic.
    For each operator, I created an if statement to perform it's intended function as an operator. I also included the vital role of the equal sign and created it's function to identify different operators. I am still not finished--still needing decimals and keep track of multiple operations, etc...

--- calculator/src/components/App/index.js
+++ calculator/src/components/App/index.js
@@ -7,13 +7,84 @@ const App = () => {
   const [value, setValue] =
     useState("0"); /* This sets the constant value of 0 */
 
+  const [memory, setMemory] =
+    useState(null);
+
+  const [operator, setOperator] =
+    useState(null);
+    
   const handleButtonPress = (content) => () => {
     const num = parseFloat(value);
+
+    if(content === "AC") {
+      setValue("0");
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if(content === "±"){
+      setValue((num * -1).toString());
+      return;
+    }
+
+    if(content === "%"){
+      setValue((num / 100).tostring());
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if(content === "+"){
+      setMemory(parseFloat(value));
+      setValue("0");
+      setOperator("+");
+      return;
+    }
+
+    if(content === "-"){
+      setMemory(parseFloat(value));
+      setValue("0");
+      setOperator("-");      
+    }
+
+    if(content === "x"){
+      setMemory(parseFloat(value));
+      setValue("0");
+      setOperator("x");      
+    }
+
+    if(content === "÷"){
+      setMemory(parseFloat(value));
+      setValue("0");
+      setOperator("÷");      
+    }
+
+    if(content === "="){
+      if(!operator) return;
+
+      if (operator === "+") {
+        setValue((memory + parseFloat(value)).toString());
+      } else if (operator === "−") {
+        setValue((memory - parseFloat(value)).toString());
+      } else if (operator === "×") {
+        setValue((memory * parseFloat(value)).toString());
+      } else if (operator === "÷") {
+        setValue((memory / parseFloat(value)).toString());
+      }
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+      setMemory(null);
+      return;
+    }
+    
 
     setValue(parseFloat(num + content).toString());
   };
 
-  return (
+  return ( /* should be an error here for now */
     <div className="App">
       <div className="top">7:77</div>
       <div className="display">{value}</div>
